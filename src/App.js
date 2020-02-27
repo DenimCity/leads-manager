50d65d772efb8d94d68a73a742b3479158e59c72
DenimCity <altidorj1@gmail.com>
2020-02-26 22:30:04 -0500

    rednering a table

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [state, setState] = useState({
     loading: true,
-    results: []
+    results: [],
+    error: false,
+    errorMessages: []
   })
 
   const fetchData = async () => {
     try {
       const results = await axios.get('/api/leads')
-      console.log("TCL: fetchData -> results", results)
+      setState({loading: !state.loading, results: results.data})
     } catch (error) {
-    console.log("TCL: fetchData -> error", error.message)
-      
+    setState({loading: !state.loading,error: true, errorMessages: [error.message]})
     }
   }
 
   useEffect(() =>{
     fetchData()
   },[])
+
+  function search(array){
+    return array.map((k, index) => (
+          <tr key={`${k.name}-${k.id}-${index}`}>
+          <td>{k.id}</td>
+          <td>{k.name}</td>
+          <td>{k.email}</td>
+          <td>{k.created_at}</td>
+          </tr>
+        )
+      )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <table style={{width:"60%"}}>
+  <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Created</th>
+  </tr>
+  
+   {search(state.results)}
+   
+ 
+</table>
+
     </div>
   );
 }
