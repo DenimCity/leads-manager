57edc11a749a333392252f4f1d764f83c3ae27e4
DenimCity <altidorj1@gmail.com>
2020-02-27 00:00:19 -0500

    ok

---
+++ backend/leads/api.py
@@ -0,0 +1,16 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    serializer_class = LeadSerializer
+
+    # def get_queryset(self):
+    #     return self.request.user.leads.all()
